6aaf66b45b7f5241e4200e68f8d2c91c863c20f9
nicolasschinagel <drivepoweringames@gmail.com>
2025-09-30 21:28:02 -0300

    config simples

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projetoBancoDS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
