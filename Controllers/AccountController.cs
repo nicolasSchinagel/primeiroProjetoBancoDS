e0794c2df66e44715714438e66c0ea2db0e77461
nicolasschinagel <drivepoweringames@gmail.com>
2025-10-07 22:24:22 -0300

    começando model para cliente

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projetoBancoDS.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult CadCli()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projetoBancoDS.Models
+{
+    public abstract class Cliente
+    {
+        [Required]
+        public int IdCliente { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string? NomeCliente { get; set; }
+
+        [Required]
+        [StringLength(9)]
+        public string? CEP { get; set; }
+
+        [Required]
+        public string? Logradouro { get; set; }
+
+        [Required]
+        public int Numero { get; set; }
+
+        [Required]
+        public string? Pais { get; set; }
+
+        [Required]
+        public string? Estado { get; set; }
+
+        [Required]
+        public string? Cidade { get; set; }
+
+        [Required]
+        public string? Bairro { get; set; }
+
+        [Required]
+        public string? NumeroTel { get; set; }
+    }
+    public class ClientePF : Cliente
+    {
+        [Required]
+        public int CPF { get; set; }
+    }
+    public class ClientePJ : Cliente
+    {
+        [Required]
+        public int CNPJ { get; set; }
+    }
+}
