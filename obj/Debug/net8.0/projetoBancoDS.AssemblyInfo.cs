b39ccda2920fc16cd528a0316f5065a2bf0e342b
nicolasschinagel <drivepoweringames@gmail.com>
2025-10-10 22:49:35 -0300

    Melhorando view CadCli

--- obj/Debug/net8.0/projetoBancoDS.AssemblyInfo.cs
+++ obj/Debug/net8.0/projetoBancoDS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projetoBancoDS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02457f475303ad7147b3189462e669a162cfce09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf6dc1324ca23648ac6044f05d86097d4fb11b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("projetoBancoDS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projetoBancoDS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
