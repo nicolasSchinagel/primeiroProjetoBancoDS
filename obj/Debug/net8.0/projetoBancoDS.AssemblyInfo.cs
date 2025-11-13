5cd76a64dd5ba6e553e82812c51d80422a035c4b
nicolasschinagel <drivepoweringames@gmail.com>
2025-11-12 22:45:21 -0300

    adicionando imagem da logo

--- obj/Debug/net8.0/projetoBancoDS.AssemblyInfo.cs
+++ obj/Debug/net8.0/projetoBancoDS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projetoBancoDS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79e8142fac82554ac6c7c2bf50584cc8e797eb39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a07551ffb7adff1fe78376461e538d41fcdd33")]
 [assembly: System.Reflection.AssemblyProductAttribute("projetoBancoDS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projetoBancoDS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
